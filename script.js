0e1c03ece13c28493351ca378017280a52c170cb
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-03-19 04:31:00 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
-    //Write your code here
+  var form = document.getElementById("form1");
+
+            // Get the input elements by their names
+            var firstNameInput = form.elements["fname"];
+            var lastNameInput = form.elements["lname"];
+
+            // Get the values from the input elements
+            var firstName = firstNameInput.value;
+            var lastName = lastNameInput.value;
+
+            // Do something with the values (for example, log them)
+            alert(firstName + " " + lastName);
+          
+
 
 }
